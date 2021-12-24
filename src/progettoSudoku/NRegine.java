cb7eab043c067da61d8d94518d5cd50d0bfe9be7
milano-007 <milanofrancesco82@gmail.com>
2021-12-24 15:15:28 +0100

    finished

---
+++ src/progettoSudoku/NRegine.java
@@ -0,0 +1,90 @@
+package progettoSudoku;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class NRegine extends Backtracking<Integer,Integer> {
+
+    private boolean [][]board;
+    private int n, nrSol=0;
+    private int regineAssegnate=0;
+
+    public NRegine(int n) {
+        if( n<=3 ) throw new IllegalArgumentException();
+        board=new boolean[n][n];
+        this.n=n;
+    }
+
+    @Override
+    protected Collection<Integer> scelte(Integer p ) {
+        List<Integer> s=new ArrayList<>();
+        for( int i=0; i<n; ++i )
+            s.add(i);
+        return s;
+    }
+
+    @Override
+    protected boolean esisteSoluzione() {
+        return regineAssegnate==n;
+    }//esisteSoluzione
+
+    @Override
+    protected boolean assegnabile( Integer r, Integer c ) {
+        //è assegnabile una regina sulla colonna c della riga r?
+        //verifica i vincoli di essere sotto attacco
+        //verifica attacco a NORD
+        for( int i=r-1; i>=0; --i )
+            if( board[i][c] ) return false;
+        //verifica attacco a NORD-OVEST
+        for( int i=r-1,j=c-1; i>=0 && j>=0; --i,--j )
+            if( board[i][j] ) return false;
+        //verifica attacco a NORD-EST
+        for( int i=r-1,j=c+1; i>=0 && j<=n-1; --i,++j )
+            if( board[i][j] ) return false;
+        return true;
+    }//assegnabile
+
+    @Override
+    protected void assegna( Integer r, Integer c ) {
+        board[r][c]=true;
+        regineAssegnate++;
+    }//assegna
+
+    @Override
+    protected void deassegna( Integer r, Integer c ) {
+        board[r][c]=false;
+        regineAssegnate--;
+    }//deassegna
+
+    @Override
+    protected void scriviSoluzione() {
+        nrSol++;
+        System.out.print(nrSol+": ");
+        for( int i=0; i<n; ++i ) {
+            for( int j=0; j<n; ++j )
+                if( board[i][j] ) {
+                    System.out.print("<"+i+","+j+">");
+                    break; //interrompe il ciclo di for interno
+                }
+        }
+        System.out.println();
+    }//scriviSoluzione
+
+    @Override
+    protected Integer prossimoPuntoDiScelta(Integer p) {
+        if (p>=n-1)
+            throw new IllegalStateException();
+        return ++p;
+    }
+
+    @Override
+    protected boolean esisteProssimoPuntoDiScelta(Integer p) {
+        return p<n-1;
+    }
+
+    public static void main( String[] args ) {
+        new NRegine(8).risolvi(0);
+    }//main
+
+}
---
+++ src/progettoSudoku/Sudoku.java
@@ -0,0 +1,166 @@
+package progettoSudoku;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Sudoku extends Backtracking<Integer, Integer> {
+
+    private int[] griglia;
+    private static final int SIZE = 9;
+
+    public Sudoku(String input) {
+        this.griglia = new int[SIZE*SIZE];
+
+        StringTokenizer st = new StringTokenizer(input, "\n ",false);
+        for(int i=0; i<SIZE*SIZE; i++) {
+            griglia[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    public Sudoku(int[][] board) {
+        griglia = new int[SIZE*SIZE];
+        int pos = 0;
+        for(int i=0; i<SIZE; i++) {
+            for(int j=0; j<SIZE; j++) {
+                griglia[pos++] = board[i][j];
+            }
+        }
+    }
+    @Override
+    protected boolean assegnabile(Integer pos, Integer scelta) {
+        if(griglia[pos] != 0)
+            return false;
+
+        //controllo orizzontale
+        int start = pos - (pos%SIZE);
+        int a;
+        for(int i=0; i<SIZE; i++) {
+            a = start + i;
+            if(griglia[a] == scelta)
+                return false;
+        }
+
+        //controllo verticale
+
+        start = pos - (SIZE * (pos/SIZE));
+        for(int i=0; i<SIZE; i++) {
+            a = start + (SIZE*i);
+            if(griglia[a] == scelta)
+                return false;
+        }
+
+        //controllo quadrato
+
+        int boxRow = pos/SIZE - (pos/SIZE) % (int) (Math.sqrt(SIZE));
+        int boxCol = pos%SIZE - (pos%SIZE) % (int) Math.sqrt(SIZE);
+
+        int tmp;
+
+        for(int r = boxRow; r<boxRow+Math.sqrt(SIZE); r++) {
+            for(int d = boxCol; d<boxCol+Math.sqrt(SIZE); d++) {
+                tmp = SIZE*r + d;
+                if(griglia[tmp] == scelta)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isFull() {
+        for(int i=0; i<SIZE*SIZE; i++)
+            if(griglia[i] == 0)
+                return false;
+        return true;
+    }
+    @Override
+    protected void assegna(Integer pos, Integer scelta) {
+        griglia[pos] = scelta;
+    }
+
+    @Override
+    protected void deassegna(Integer pos, Integer integer) {
+        griglia[pos] = 0;
+    }
+
+    @Override
+    protected void scriviSoluzione() {
+        for(int i=0; i<SIZE; i++) {
+            for(int j=0; j<SIZE; j++) {
+                System.out.print(griglia[SIZE*i + j]);
+                if(j != 8)
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    @Override
+    protected boolean esisteSoluzione() {
+        return isFull();
+    }
+
+    @Override
+    protected Integer prossimoPuntoDiScelta(Integer pos) {
+        if(pos >= SIZE*SIZE-1)
+            throw new IllegalStateException();
+        for(;;pos++) {
+            if(griglia[pos] == 0)
+                return pos;
+        }
+    }
+
+    @Override
+    protected boolean esisteProssimoPuntoDiScelta(Integer pos) {
+        for(;pos<SIZE*SIZE; pos++) {
+            if(griglia[pos] == 0)
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected Collection<Integer> scelte(Integer integer) {
+        List<Integer> lista = new ArrayList<>();
+        for(int i=1; i<=SIZE; i++) {
+            lista.add(i);
+        }
+        return lista;
+    }
+
+    protected Integer primoPuntoDiScelta() {
+        for(int pos=0; pos < SIZE*SIZE-1; pos++) {
+            if(griglia[pos] == 0)
+                return pos;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] board = new int[][] {
+                { 0, 0, 0, 8, 0, 0, 4, 2, 0 },
+                { 5, 0, 0, 6, 7, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 9, 0, 0, 5 },
+                { 7, 4, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 9, 0, 3, 0, 7, 0, 0 },
+                { 0, 0, 0, 0, 0, 7, 0, 4, 8 },
+                { 8, 0, 0, 4, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 9, 8, 0, 0, 3 },
+                { 0, 9, 5, 0, 0, 3, 0, 0, 0 }
+        };
+        /*
+        { 6, 3, 1, 2, 5, 8, 9, 7, 4 },
+        { 8, 2, 4, 7, 9, 1, 6, 5, 3 },
+        { 5, 7, 9, 4, 3, 6, 2, 1, 8 },
+        { 4, 6, 2, 5, 8, 3, 1, 9, 7 },
+        { 7, 1, 8, 9, 2, 4, 5, 3, 6 },
+        { 3, 9, 5, 1, 6, 7, 8, 4, 2 },
+        { 1, 8, 6, 3, 7, 5, 4, 2, 9 },
+        { 9, 5, 3, 8, 4, 2, 7, 6, 1 },
+        { 2, 4, 7, 6, 1, 9, 3, 0, 0 }
+        */
+        Sudoku s = new Sudoku(board);
+        s.risolvi(s.primoPuntoDiScelta());
+    }
+}
