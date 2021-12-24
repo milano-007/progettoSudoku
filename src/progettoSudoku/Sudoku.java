23bb4550679855033f05bcc6303cd3c53e518af9
milano-007 <milanofrancesco82@gmail.com>
2021-12-24 15:59:31 +0100

    finished

--- src/progettoSudoku/Sudoku.java
+++ src/progettoSudoku/Sudoku.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Sudoku extends Backtracking<Integer, Integer> {
 
-    private int[] griglia;
+    private final int[] griglia;
     private static final int SIZE = 9;
 
     public Sudoku(String input) {
@@ -139,15 +139,15 @@ public class Sudoku extends Backtracking<Integer, Integer> {
 
     public static void main(String[] args) {
         int[][] board = new int[][] {
-                { 0, 0, 0, 8, 0, 0, 4, 2, 0 },
-                { 5, 0, 0, 6, 7, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 9, 0, 0, 5 },
-                { 7, 4, 0, 1, 0, 0, 0, 0, 0 },
-                { 0, 0, 9, 0, 3, 0, 7, 0, 0 },
-                { 0, 0, 0, 0, 0, 7, 0, 4, 8 },
-                { 8, 0, 0, 4, 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 9, 8, 0, 0, 3 },
-                { 0, 9, 5, 0, 0, 3, 0, 0, 0 }
+                { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+                { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                { 0, 0, 5, 2, 0, 6, 3, 0, 0 }
         };
         /*
         { 6, 3, 1, 2, 5, 8, 9, 7, 4 },
